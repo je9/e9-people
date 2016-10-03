ed6ff891d6ff9f6fe0c2a2566b8cdc386b01365a
Justin Taylor <justin@elevate9.co.nz>
2016-10-03 16:22:06 +1300

    initial commit

---
+++ e9-people.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Plugin Name: e9-people
+ * GitHub Plugin URI: je9/e9-people
+ * Description: E9 Person CPT and team page for WordPress.
+ * Version: 0.1.0
+ * Author: justin@e9.nz
+ * Author URI: http://e9.nz
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * RequiresWP: 4.6
+ * RequiresPHP: 5.6.23
+*/
+
+// create person custom post type
+
+// add meta fields
+
+// need a page template
